c6fe932856f50c387daa3f1d587f2e0189b25b58
James Hutt <jhutt@g.harvard.edu>
2020-04-03 11:21:15 +0100

    Plot resized and page reorganised

--- final_project/app.R
+++ final_project/app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 # Load necessary libraries
 
 library(shiny)
@@ -25,21 +16,15 @@ ui <- fluidPage(
     # Application title
     titlePanel("State Network"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("scale",
-                        "Size of Nodes:",
-                        min = 0,
-                        max = 200,
-                        value = 100)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("netwPlot")
+    # Show a plot of the generated distribution
+    mainPanel(
+              sliderInput("scale",
+                   "Size of Nodes:",
+                   min = 0,
+                   max = 200,
+                   value = 100), 
+              plotOutput("netwPlot"), 
         )
-    )
 )
 
 # Define server logic required to network plot
@@ -47,7 +32,7 @@ server <- function(input, output) {
 
     output$netwPlot <- renderPlot({
         plot_network(scale_factor = (input$scale / 100))
-    }, height = 600, width = 900)
+    }, height = 500, width = 750)
 }
 
 # Run the application 
